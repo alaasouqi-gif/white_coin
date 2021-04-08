177c9db46c5f256203406857c9e2c5131b8f28a4
alaa-souqi <alaa3souiq@gmail.com>
2021-04-08 03:33:50 +0300

    log in widget

--- lib/Screens/SignUp.dart
+++ lib/Screens/SignUp.dart
@@ -9,13 +9,15 @@ class SignUp extends StatefulWidget {
 
 class _SignUpState extends State<SignUp> {
   String email;
+  String name;
+  String pass;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
           body: Padding(
-            padding: EdgeInsets.only(top: 32, bottom: 16, left: 16, right: 16),
+            padding: EdgeInsets.only(top: 16, bottom: 16, left: 16, right: 16),
             child: Column(
               mainAxisSize: MainAxisSize.max,
               crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -25,7 +27,7 @@ class _SignUpState extends State<SignUp> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      'Welcome,',
+                      'Create account,',
                       style: TextStyle(
                         color: Colors.black,
                         fontSize: 32,
@@ -33,7 +35,7 @@ class _SignUpState extends State<SignUp> {
                       ),
                     ),
                     Text(
-                      'Sign in to continue!',
+                      'Sign up to get started!',
                       style: TextStyle(
                         color: KSecondColor,
                         fontSize: 22,
@@ -47,35 +49,51 @@ class _SignUpState extends State<SignUp> {
                   children: [
                     TextFormField(
                       onChanged: (value) {
-                        email = value;
+                        name = value;
                       },
+                      keyboardType: TextInputType.name,
                       textInputAction: TextInputAction.next,
                       decoration: KDecoration.copyWith(labelText: 'Full Name'),
                     ),
                     SizedBox(
-                      height: 16,
+                      height: 14,
                     ),
                     TextFormField(
+                      keyboardType: TextInputType.emailAddress,
                       onChanged: (value) {
                         email = value;
                       },
                       textInputAction: TextInputAction.next,
                       decoration: KDecoration.copyWith(labelText: 'Email'),
                     ),
+
                     SizedBox(
-                      height: 16,
+                      height: 14,
                     ),
                     TextFormField(
+                      keyboardType: TextInputType.phone,
                       onChanged: (value) {
                         email = value;
                       },
                       textInputAction: TextInputAction.next,
+                      decoration: KDecoration.copyWith(labelText: 'Phone'),
+                    ),
+                    SizedBox(
+                      height: 14,
+                    ),
+
+                    TextFormField(
+                      keyboardType: TextInputType.visiblePassword,
+                      onChanged: (value) {
+                        pass = value;
+                      },
+                      textInputAction: TextInputAction.done,
                       decoration: KDecoration.copyWith(labelText: 'Password'),
                     ),
                   ],
                 ),
                 MyButton(
-                  text: 'Login',
+                  text: 'Sign Up',
                   onTap: () {},
                 )
 
